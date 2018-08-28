94be65e9b6099744bdac5130b2742f18a9f5f7f9
Michael Lin <michaellin@Michaels-MacBook-Pro-3.local>
2018-08-28 15:14:20 -0700

    implement handleVideoSearch

--- src/actions/search.js
+++ src/actions/search.js
@@ -7,6 +7,12 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 var handleVideoSearch = (q) => {
  
   //TODO:  Write an asynchronous action to handle a video search!
+  const cb = videos => {
+    changeVideoList(videos);
+    changeVideo(videos[0]);
+  }
+  searchYouTube({key: YOUTUBE_API_KEY, query: q}, cb);
+  return cb;
 };
 
 export default handleVideoSearch;
