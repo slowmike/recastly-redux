3f53932062ab84ec128eeafc1a756d6f7982e91a
Michael Lin <michaellin@Michaels-MacBook-Pro-3.local>
2018-08-28 20:50:37 -0700

    added reducer to videoList

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,8 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (state = [], action) => ({
   //TODO: define a reducer for the videoList field of our state.
-};
+  'CHANGE_VIDEO_LIST': action.videos,
+}[action.type] || state);
 
 export default videoListReducer;
